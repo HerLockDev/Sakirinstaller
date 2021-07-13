aea0641f307d24d80ba258d00186fb08a3e56794
SakirBey1 <86895448+SakirBey1@users.noreply.github.com>
2021-07-13 20:08:20 +0300

    Update __main__.py

--- sakir_installer/__main__.py
+++ sakir_installer/__main__.py
@@ -4,7 +4,7 @@ from telethon import TelegramClient, functions
 from telethon.sessions import StringSession
 from rich.prompt import Prompt, Confirm
 from asyncio import get_event_loop
-from siri_installer import *
+from sakir_installer import *
 from .astring import main
 from time import time
 from . import console
@@ -28,7 +28,7 @@ def connect (api):
     return heroku_conn
 
 def createApp (connect):
-    appname = "siriuserbot" + str(time() * 1000)[-4:].replace(".", "") + str(random.randint(0,500))
+    appname = "sakiruserbot" + str(time() * 1000)[-4:].replace(".", "") + str(random.randint(0,500))
     try:
         connect.create_app(name=appname, stack_id_or_name='container', region_id_or_name="eu")
     except requests.exceptions.HTTPError:
@@ -63,14 +63,14 @@ async def oturumacvebotlogolustur (stri, aid, ahash):
         await Client.start()
         ms = await Client.send_message('me',LANG['SIRIUSERBOT'])
         KanalId = await Client(CreateChannelRequest(
-            title='SiriUserBot BotLog',
+            title='SakirUserBot BotLog',
             about=LANG['AUTO_BOTLOG'],
             megagroup=True
         ))
 
         KanalId = KanalId.chats[0].id
 
-        Photo = await Client.upload_file(file='20210701_125852.jpg')
+        Photo = await Client.upload_file(file='sakıruserbot.jpg')
         await Client(EditPhotoRequest(channel=KanalId, 
             photo=Photo))
         msg = await Client.send_message(KanalId, LANG['DONT_LEAVE'])
@@ -97,7 +97,7 @@ if __name__ == "__main__":
     onemli(LANG['GETTING_STRING_SESSION'])
     stri, aid, ahash = main()
     basarili(LANG['SUCCESS_STRING'])
-    SyperStringKey = "SiriUserBot"
+    SyperStringKey = "SakirUserBot"
     baslangic = time()
 
 
@@ -107,14 +107,14 @@ if __name__ == "__main__":
     basarili(LANG['SUCCESS_APP'])
     onemli(LANG['DOWNLOADING'])
 
-    SyperStringKey = "SiriUserBot"
-    GiperStringKey = "SiriUserBot/"
+    SyperStringKey = "SakirUserBot"
+    GiperStringKey = "SakirUserBot/"
     InvalidKey = "http://github.com/" 
     str1 = InvalidKey+GiperStringKey+SyperStringKey
 
-    if os.path.isdir("./SiriUserBot/"):
-        rm_r("./SiriUserBot/")
-    repo = Repo.clone_from(str1,"./SiriUserBot/", branch="master")
+    if os.path.isdir("./SakirUserBot/"):
+        rm_r("./SakirUserBot/")
+    repo = Repo.clone_from(str1,"./SakirUserBot/", branch="master")
     onemli(LANG['DEPLOYING'])
     app = hgit(heroku, repo, appname)
     config = app.config()
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     config['CLEAN_WELCOME'] = "True"
     config['CONSOLE_LOGGER_VERBOSE'] = "False"
     config['COUNTRY'] = COUNTRY
-    config['DEFAULT_BIO'] = "✨ @SiriUserBot"
+    config['DEFAULT_BIO'] = "✨ @SakirUserBot1"
     config['DEFAULT_NAME'] = "Sahip"
     config['LANGUAGE'] = LANGUAGE
     config['GALERI_SURE'] = "60"
@@ -146,7 +146,7 @@ if __name__ == "__main__":
     config['TMP_DOWNLOAD_DIRECTORY'] = "./downloads/"
     config['TZ'] = TZ
     config['TZ_NUMBER'] = "1"
-    config['UPSTREAM_REPO_URL'] = "https://github.com/SiriUserBot/SiriUserBot"
+    config['UPSTREAM_REPO_URL'] = "https://github.com/SakirBey1/SakirUserBot"
 
     config['WARN_LIMIT'] = "3"
     config['WARN_MODE'] = "gmute"
