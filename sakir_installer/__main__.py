b5bf5775b709236048560cbf03683079a246f9f8
SakirBey1 <86895448+SakirBey1@users.noreply.github.com>
2021-07-14 08:52:06 +0300

    Update __main__.py

--- sakir_installer/__main__.py
+++ sakir_installer/__main__.py
@@ -107,7 +107,7 @@ if __name__ == "__main__":
     basarili(LANG['SUCCESS_APP'])
     onemli(LANG['DOWNLOADING'])
 
-   SyperStringKey = "Sakiruserbot"
+   SyperStringKey = "SakirUserBot"
     GiperStringKey = "Sakirbey1/"
     InvalidKey = "http://github.com/" 
     str1 = InvalidKey+GiperStringKey+SyperStringKey
